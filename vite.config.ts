4532636bb84cba25cdf73870632dea196568f7ea
thousands.of.ties <thousands.of.ties@gmail.com>
2026-07-19 05:41:33 +0900

    Set TutoTuto IndexedDB name

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,8 @@ export default defineConfig(({ mode }) => {
   return {
     base: basePath,
     define: {
-      'import.meta.env.VITE_APP_COMMIT_HASH': JSON.stringify(commitHash)
+      'import.meta.env.VITE_APP_COMMIT_HASH': JSON.stringify(commitHash),
+      'import.meta.env.VITE_INDEXED_DB_NAME': JSON.stringify('TutoTutoDB')
     },
     resolve: {
       alias: {
